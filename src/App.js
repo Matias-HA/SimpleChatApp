4b6f3dd0ab0c8bd483981116ef0d28406ba44bab
Matias-HA <matias.hougaard.andreasen@gmail.com>
2021-09-27 00:24:36 +0200

    Add splash screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 // Libraries
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
 import {Provider} from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
 
 // Includes
 import Navigation from './navigation';
 import {store} from './shared/context/store';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <SafeAreaView style={{flex: 1}}>
